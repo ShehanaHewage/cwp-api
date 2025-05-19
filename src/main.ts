fab5912e72c465ff30eca6d730b031a409aff5e1
ShehanaHewage <ehkshehana@gmail.com>
2025-05-19 15:27:19 +0530

    added api end points

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import { Hono } from 'hono'
 import dotenv from 'dotenv'
 import logger from './utils/logger.ts'
 import { prettyJSON } from 'hono/pretty-json'
-
 import healthRoutes from './handlers/health.ts'
 import userRoutes from './handlers/user.ts'
 import uploadRoute from './handlers/upload.ts'
@@ -11,8 +10,9 @@ import fileRoutes from './handlers/fileHandler.ts'
 import postRoutes from './handlers/posts.ts';
 dotenv.config()
 
+import { cors } from 'hono/cors'
 const app = new Hono()
-
+app.use("/api/*", cors());
 // Middleware for logging requests
 // app.use('*', async (c, next) => {
 //   const start = Date.now()
@@ -35,7 +35,7 @@ app.route("/api", uploadRoute)
 app.route('/api', fileRoutes)
 app.route('/api/posts', postRoutes)
 
-const port = parseInt(process.env.PORT || '3000')
+const port = parseInt(process.env.PORT || '8080')
 logger.info(`Server is running on http://localhost:${port}`)
 
 serve({
